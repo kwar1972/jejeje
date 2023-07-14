bb1f8d23fcc8229f989818e121d792eafe594e97
Ariel Eduardo Ghysels <arieleduardoghysels@Ariels-MacBook-Pro.local>
2023-07-14 13:10:31 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[subs, setSubs] = useState([
+    {
+      nick: 'dapelu', 
+      subMonths: 3,
+      avatar: 'https://i.pravatar.cc/150?u=dapelu',
+      description: "Dapelo hace de moderador a veces."
+    },
+    {
+      nick: 'sergio_serrano', 
+      subMonths: 7,
+      avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
+      description: "Dapelo hace de moderador a veces."
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ariel Subs</h1>
+      <ul>
+        {
+          subs.map(sub => {
+            return (
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt={'avatar for ${sub.nick}'} />
+                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0, 100)}</p>     
+              </li>
+            )
+          }
+          )
+        }
+      </ul>
     </div>
   );
 }
